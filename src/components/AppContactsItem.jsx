14f352de26ad41c818ac31eb5e24405c2ceadb21
Urichcool <104870932+Urichcool@users.noreply.github.com>
2023-01-04 16:00:51 +0100

    fixed-delete-button

--- src/components/AppContactsItem.jsx
+++ src/components/AppContactsItem.jsx
@@ -1,15 +1,20 @@
 import { ContactsListItem, ContactsDeleteButton } from './App,styled';
 import PropTypes from 'prop-types';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { deleteContact } from 'redux/operations';
+import { getIsLoading } from 'redux/contactsSlice';
 
 const AppContactsItem = ({ name, phone, id }) => {
   const dispatch = useDispatch();
+  const isLoading = useSelector(getIsLoading);
 
   return (
     <ContactsListItem>
       {name}: {phone}
-      <ContactsDeleteButton onClick={() => dispatch(deleteContact(id))}>
+      <ContactsDeleteButton
+        onClick={() => dispatch(deleteContact(id))}
+        disabled={isLoading}
+      >
         Delete
       </ContactsDeleteButton>
     </ContactsListItem>
